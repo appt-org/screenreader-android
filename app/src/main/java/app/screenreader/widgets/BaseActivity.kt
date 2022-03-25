8e0363620f2c12e028bd127c55879405d530e850
Jan Jaap de Groot <info@janjaap.com>
2022-03-25 17:10:36 +0100

    Improve toolbar accessibility

--- app/src/main/java/app/screenreader/widgets/BaseActivity.kt
+++ app/src/main/java/app/screenreader/widgets/BaseActivity.kt
@@ -1,7 +1,14 @@
 package app.screenreader.widgets
 
 import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.children
+import app.screenreader.R
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 /**
@@ -12,5 +19,16 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Improve accessibility of Toolbar
+        findViewById<Toolbar>(R.id.toolbar).children.firstOrNull {
+            it is TextView
+        }?.let { titleView ->
+            ViewCompat.setAccessibilityHeading(titleView, true)
+        }
     }
 }
