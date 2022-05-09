92e20031c226abde36a34e65bd3d33e91d157a44
Jan Jaap de Groot <info@janjaap.com>
2022-05-09 11:02:43 +0200

    Update action instructions

--- app/src/main/java/app/screenreader/views/actions/LinkTextView.kt
+++ app/src/main/java/app/screenreader/views/actions/LinkTextView.kt
@@ -1,19 +1,11 @@
 package app.screenreader.views.actions
 
 import android.content.Context
-import android.text.SpannableString
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.text.style.UnderlineSpan
 import android.text.util.Linkify
 import android.util.AttributeSet
-import android.view.View
 import android.view.accessibility.AccessibilityEvent
 import android.widget.TextView
-import android.widget.Toast
-
 
 /**
  * Created by Jan Jaap de Groot on 18/11/2020
@@ -30,28 +22,6 @@ class LinkTextView @JvmOverloads constructor(
         movementMethod = LinkMovementMethod.getInstance()
         linksClickable = false
         isClickable = false
-
-//        val spannableStringBuilder = SpannableStringBuilder("Please go to \"Settings\" in")
-//        val spannableString = SpannableString("mywebsite.com")
-//
-//        val clickableSpan: ClickableSpan = object : ClickableSpan() {
-//            override fun onClick(view: View) {
-//                // Ignored
-//            }
-//        }
-//
-//        spannableString.setSpan(
-//            clickableSpan,
-//            0,
-//            spannableString.length,
-//            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-//        )
-//        spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, 0)
-//
-//        spannableStringBuilder.append(spannableString)
-//        spannableStringBuilder.append("to change this status")
-//
-//        text = spannableStringBuilder
     }
 
     override fun dispatchPopulateAccessibilityEvent(event: AccessibilityEvent?): Boolean {
